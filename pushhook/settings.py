0fc39247de8a67b463ec7fb789adb1759abb21b9
root <root@ip-172-31-17-99.us-west-2.compute.internal>
2017-03-17 06:17:00 +0000

    production build debug false

--- pushhook/settings.py
+++ pushhook/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '032(mcea-+fhg)=r3imb^0v=zo==9c9cyu_n0(_@%qhmunk&2q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.ngrok.io','54.213.246.126']
 
